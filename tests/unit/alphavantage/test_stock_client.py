9963d378969fe84f8f7dd769127a78a47377a61a
adam <kjharper93@gmail.com>
2020-07-03 12:27:03 +0100

    flattened finpy package structure

---
+++ tests/unit/alphavantage/test_stock_client.py
@@ -0,0 +1,146 @@
+import unittest
+from unittest import mock
+from finpy import alphavantage as av
+
+class TestStockClient(unittest.TestCase):
+    def setUp(self):
+        req_path = 'finpy.alphavantage.requests.Session.get'
+        self.mock_get_patcher = mock.patch(req_path)
+        self.mock_get = self.mock_get_patcher.start()
+        self.mock_get.return_value.text = r'{"some":"json"}'
+
+        mock_key = '0000000000'
+        self.client = av.StockClient(mock_key)
+
+    def test_get_intraday_with_default_params(self):
+        """ test that a valid intraday request with
+        default kwargs returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_intraday_data('MSFT')
+        self.assertEqual(data, expected)
+
+    def test_get_daily_with_default_params(self):
+        """ test that a valid daily request with
+        default kwargs returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_daily_data('MSFT')
+        self.assertEqual(data, expected)
+
+    def test_get_daily_adjusted_with_default_params(self):
+        """ test that a valid daily adjusted request with
+        default kwargs returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_daily_adjusted_data('MSFT')
+        self.assertEqual(data, expected)
+
+    def test_get_intraday_with_str_params(self):
+        """ test that a valid intraday request with
+        string args returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_intraday_data(
+            'MSFT',
+            interval='5min',
+            output_size='full',
+            data_type='csv'
+        )
+        self.assertEqual(data, expected)
+
+    def test_get_daily_with_str_params(self):
+        """ test that a valid daily request with
+        string args returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_daily_data(
+            'MSFT',
+            output_size='full',
+            data_type='csv'
+        )
+        self.assertEqual(data, expected)
+
+    def test_get_daily_adjusted_with_str_params(self):
+        """ test that a valid daily adjusted request with
+        string args returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_daily_adjusted_data(
+            'MSFT',
+            output_size='full',
+            data_type='csv'
+        )
+        self.assertEqual(data, expected)
+
+    def test_get_intraday_with_enum_params(self):
+        """ test that a valid intraday request with
+        enum args returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_intraday_data(
+            'MSFT',
+            interval=av.Interval.M1,
+            output_size=av.OutputSize.FULL,
+            data_type=av.DataType.CSV
+        )
+        self.assertEqual(data, expected)
+
+    def test_get_daily_with_enum_params(self):
+        """ test that a valid daily request with
+        string args returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_daily_data(
+            'MSFT',
+            output_size=av.OutputSize.FULL,
+            data_type=av.DataType.CSV
+        )
+        self.assertEqual(data, expected)
+
+    def test_get_daily_adjusted_with_enum_params(self):
+        """ test that a valid daily adjusted request with
+        string args returns data. """
+
+        expected = self.mock_get.return_value.text
+        data = self.client.get_daily_adjusted_data(
+            'MSFT',
+            output_size=av.OutputSize.FULL,
+            data_type=av.DataType.CSV
+        )
+        self.assertEqual(data, expected)
+
+    def test_get_intraday_with_invalid_params(self):
+        """ test that invalid intraday requests raise
+        value errors. """
+
+        with self.assertRaises(ValueError):
+            self.client.get_intraday_data('MSFT', interval='2min')
+        with self.assertRaises(ValueError):
+            self.client.get_intraday_data('MSFT', output_size='big')
+        with self.assertRaises(ValueError):
+            self.client.get_intraday_data('MSFT', data_type='xml')
+
+    def test_get_daily_with_invalid_params(self):
+        """ test that invalid daily requests raise
+        value errors. """
+
+        with self.assertRaises(ValueError):
+            self.client.get_daily_data('MSFT', output_size='big')
+        with self.assertRaises(ValueError):
+            self.client.get_daily_data('MSFT', data_type='xml')
+
+    def test_get_daily_adjusted_with_invalid_params(self):
+        """ test that invalid daily adjusted requests raise
+        value errors. """
+
+        with self.assertRaises(ValueError):
+            self.client.get_daily_adjusted_data('MSFT', output_size='big')
+        with self.assertRaises(ValueError):
+            self.client.get_daily_adjusted_data('MSFT', data_type='xml')
+
+    def tearDown(self):
+        self.mock_get_patcher.stop()
+
+if __name__ == '__main__':
+    unittest.main()
