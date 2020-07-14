8f2adaec31cfabb171009077584017440f268774
adam <kjharper93@gmail.com>
2020-07-14 23:03:39 +0100

    removed technical indicator request from finnhub client
    added docstrings to all finhub client functions

--- finpy/finnhub.py
+++ finpy/finnhub.py
@@ -133,6 +133,17 @@ class StockClient():
         return response.text
 
     def get_basic_financials(self, symbol, metric='all'):
+        """ Get basic financial data on a company.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+            metric (str): Metrics to retrieve. Can be one of
+                all, price, valuation, margin.
+
+        Returns:
+            str: A json string containing company financial data.
+        """
+
         endpoint = 'stock/metric'
         params = {
             'symbol': symbol,
@@ -145,6 +156,15 @@ class StockClient():
         return response.text
 
     def get_financials_as_reported(self, symbol):
+        """ Get financials as reported for a company.
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing company financials
+                as reported data.
+        """
+
         endpoint = 'stock/financials-reported'
         params = {'symbol': symbol}
         response = self._session.get(
@@ -154,6 +174,15 @@ class StockClient():
         return response.text
 
     def get_filings(self, symbol):
+        """ Get company filings. Limited to 250 documents at a
+        time.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing company filing data.
+        """
         endpoint = 'stock/filings'
         params = {'symbol': symbol}
         response = self._session.get(
@@ -163,6 +192,15 @@ class StockClient():
         return response.text
 
     def get_ipo_calendar(self, start, end):
+        """ Get recent and upcoming IPOs.
+
+        Args:
+            start (str): The start date in the form YYYY-DD-MM.
+            end (str): The end date in the form YYYY-DD-MM
+
+        Returns:
+            str: A json string containing IPO calendar data.
+        """
         endpoint = 'calendar/ipo'
         params = {'from': start, 'to': end}
         response = self._session.get(
@@ -172,6 +210,16 @@ class StockClient():
         return response.text
 
     def get_recommendation_trends(self, symbol):
+        """ Get latest analyst recommendation trends for
+        a company.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing recommendation trend
+                data.
+        """
         endpoint = 'stock/recommendation'
         params = {'symbol': symbol}
         response = self._session.get(
@@ -181,6 +229,14 @@ class StockClient():
         return response.text
 
     def get_price_target(self, symbol):
+        """ Get latests price target consensus.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing price target data.
+        """
         endpoint = 'stock/price-target'
         params = {'symbol': symbol}
         response = self._session.get(
@@ -190,6 +246,14 @@ class StockClient():
         return response.text
 
     def get_eps_surprises(self, symbol):
+        """ Get historic quarterly eps surprises.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing eps surprise data.
+        """
         endpoint = 'stock/earnings'
         params = {'symbol': symbol}
         response = self._session.get(
@@ -199,8 +263,16 @@ class StockClient():
         return response.text
 
     def get_earnings_calendar(self, symbol):
+        """ Get historical and upcoming earnings data.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing earnings calendar data.
+        """
         endpoint = 'calendar/earnings'
-        params = {'symbol': symbol}
+        params = {'symbol': symbol, 'international': True}
         response = self._session.get(
             self.API_URL + endpoint,
             params=params
@@ -208,6 +280,14 @@ class StockClient():
         return response.text
 
     def get_quote(self, symbol):
+        """ Get real time quotes for a stock. US stocks only.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+
+        Returns:
+            str: A json string containing quote data.
+        """
         endpoint = 'quote'
         params = {'symbol': symbol}
         response = self._session.get(
@@ -217,6 +297,19 @@ class StockClient():
         return response.text
 
     def get_candles(self, symbol, interval, start, end, adjusted=False):
+        """ Get candlestick data for a company.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+            interval (str): candlestick size. can be 1, 5, 15, 30 or 60 mins
+                or daily (D), weekly (w), monthly (m).
+            start (int): unix timestamp of the start date.
+            end (int): unix timestamp of the end date.
+            adjusted (bool): True for adjusted data, False otherwise.
+
+        Returns:
+            str: A json string containing candlestick data.
+        """
         endpoint = 'stock/candle'
         params = {
             'symbol': symbol,
@@ -231,6 +324,15 @@ class StockClient():
         return response.text
 
     def get_splits(self, symbol, start, end):
+        """ Get splits for a stock.
+        Args:
+            symbol (str): The symbol for the target stock.
+            start (str): The start date in the form YYYY-MM-DD
+            end (str): The end date in the form YYYY-MM-DD
+
+        Returns:
+            str: A json string containing splits data.
+        """
         endpoint = 'stock/split'
         params = {
             'symbol': symbol,
@@ -244,6 +346,16 @@ class StockClient():
         return response.text
 
     def get_patterns(self, symbol, interval):
+        """ Identifies candlestick patterns for a stock
+
+        Args:
+            symbol (str): The symbol for the target stock.
+            interval (str): The candlestick size. One of 1,
+                5, 15, 30, 60, D, W, M
+
+        Returns:
+            str: A json string containing pattern data.
+        """
         endpoint = 'scan/pattern'
         params = {
             'symbol': symbol,
@@ -256,6 +368,17 @@ class StockClient():
         return response.text
 
     def get_support_resistance(self, symbol, interval):
+        """ Get support and resistance levels for a stock.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+            interval (str): The candlestick size. One of 1,
+                5, 15, 30, 60, D, W, M
+
+        Returns:
+            str: A json string containing support and resistance
+                data.
+        """
         endpoint = 'scan/support-resistance'
         params = {
             'symbol': symbol,
@@ -268,28 +391,23 @@ class StockClient():
         return response.text
 
     def get_aggregate_signal(self, symbol, interval):
+        """ Get the aggregate signal of multiple technical
+        indicators.
+
+        Args:
+            symbol (str): The symbol for the target stock.
+            interval (str): The candlestick size. One of 1,
+                5, 15, 30, 60, D, W, M
+
+        Returns:
+            str: A json string containing aggregate signal
+                data.
+        """
         endpoint = 'scan/technical-indicator'
         params = {
             'symbol': symbol,
             'resolution': interval
         }
-        response = self._session.get(
-            self.API_URL + endpoint,
-            params=params
-        )
-        return response.text
-
-    def get_indicator(self, symbol, interval, start, end, indicator, indicator_fields={}):
-        endpoint = 'indicator'
-        params = {
-            'symbol': symbol,
-            'resolution': interval,
-            'from': start,
-            'to': end,
-            'indicator': indicator
-        }
-        params.update(indicator_fields)
-        print(params)
         response = self._session.get(
             self.API_URL + endpoint,
             params=params
